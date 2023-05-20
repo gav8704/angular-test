11659f8e2807598725b45e6d2158b2ed52ef9df2
Alexander Gushchin <gav8704@mail.ru>
2023-05-20 15:31:32 +0300

    creating list module

---
+++ src/app/features/notes/notes.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NotesRoutingModule } from './notes-routing.module';
+import { NotesListComponent } from './pages/notes-list/notes-list.component';
+
+@NgModule({
+  declarations: [NotesListComponent],
+  imports: [CommonModule, NotesRoutingModule],
+})
+export class NotesModule {}
