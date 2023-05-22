c63c421501f99863b70d8ca04cd2cb93c4b02c1e
Alexander Gushchin <gav8704@mail.ru>
2023-05-22 10:54:03 +0300

    split into creating and editing notes

--- src/app/features/notes/notes-routing.module.ts
+++ src/app/features/notes/notes-routing.module.ts
@@ -2,11 +2,27 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { NotesDisplayComponent } from './pages/notes-display/notes-display.component';
+import { NoteCreatorComponent } from './pages/note-creator/note-creator.component';
+import { NoteEditorComponent } from './pages/note-editor/note-editor.component';
+import { notesResolver } from './notes.resolver';
 
 const routes: Routes = [
   {
     path: 'notes',
     component: NotesDisplayComponent,
+    resolve: {
+      notes: notesResolver,
+    },
+    children: [
+      {
+        path: 'create',
+        component: NoteCreatorComponent,
+      },
+      {
+        path: 'edit/:noteId',
+        component: NoteEditorComponent,
+      },
+    ],
   },
 ];
 
---
+++ src/app/features/notes/pages/note-creator/note-creator.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-note-creator',
+  templateUrl: './note-creator.component.html',
+  styleUrls: ['./note-creator.component.scss']
+})
+export class NoteCreatorComponent {
+
+}
